8c98be3e0a4f0bcb1f4ab8208207debd1a02d062
Dr. Jeremy Simon Thornton <ifknot@hotmail.com>
2021-02-09 13:12:18 +0000

    set up SFML

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 
+#include <SFML/Graphics.hpp>
+
 int main() {
 
-  std::cout << "Orthogonal Dungeon\n";
+	std::cout << "Orthogonal Dungeon\n";
 
+	sf::RenderWindow window(sf::VideoMode(640, 480), "Orthogonal Dungeon");
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+    }
+
+	getchar();
 }
